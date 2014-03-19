511587bd099bdc1949988d665dc77b198edc5026
Rich Lane <rlane@bigswitch.com>
2014-03-19 12:11:39 -0700

    debug_counter: increase maximum name size to 64 bytes

--- modules/debug_counter/module/src/debug_counter.c
+++ modules/debug_counter/module/src/debug_counter.c
@@ -28,7 +28,7 @@ debug_counter_register(debug_counter_t *counter, const char *name, const char *d
     counter->value = 0;
     counter->counter_id = debug_counter_next_id++;
     list_push(&debug_counters, &counter->links);
-    AIM_ASSERT(strlen(name) > 0 && strlen(name) < 32);
+    AIM_ASSERT(strlen(name) > 0 && strlen(name) < 64);
     counter->name = name;
     AIM_ASSERT(strlen(description) > 0 && strlen(description) < 256);
     counter->description = description;
