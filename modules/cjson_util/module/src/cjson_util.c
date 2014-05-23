85fafcb00c0c3970504d700b675015671cdc09a1
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-23 15:23:59 -0700

    Use aim_zmalloc().

--- modules/cjson_util/module/src/cjson_util.c
+++ modules/cjson_util/module/src/cjson_util.c
@@ -1,21 +1,21 @@
 /************************************************************
  * <bsn.cl fy=2014 v=epl>
- * 
- *           Copyright 2014 Big Switch Networks, Inc.          
- * 
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  ************************************************************
  *
@@ -69,10 +69,10 @@ cjson_util_vlookup(cJSON* root, cJSON** result, const char* fmt, va_list vargs)
 
     if(root) {
         int size = vsnprintf_size__(fmt, vargs);
-        char* path = malloc(size+1);
+        char* path = aim_malloc(size+1);
         vsnprintf(path, size+1, fmt, vargs);
         rv = cjson_util_lookup_path__(root, result, path);
-        free(path);
+        aim_free(path);
     }
 
     return rv;
