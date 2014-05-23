788ec2af65e2765b3f2db31d66db89ae9bb7f021
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2014-05-23 15:23:47 -0700

    Remove incorrect comments.

--- modules/cjson_util/module/inc/cjson_util/cjson_util.h
+++ modules/cjson_util/module/inc/cjson_util/cjson_util.h
@@ -1,21 +1,21 @@
 /************************************************************
  * <bsn.cl fy=2014 v=epl>
- * 
- *           Copyright 2014 Big Switch Networks, Inc.          
- * 
+ *
+ *           Copyright 2014 Big Switch Networks, Inc.
+ *
  * Licensed under the Eclipse Public License, Version 1.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *        http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
  * License.
- * 
+ *
  * </bsn.cl>
  ********************************************************//**
  *
@@ -36,7 +36,6 @@
  * @param rv Receives the node if successful.
  * @param fmt Path format string.
  * @param ... Path format args.
- * @note Pass root=NULL for the global root.
  */
 int cjson_util_lookup(cJSON* root, cJSON** rv, const char* fmt, ...);
 
@@ -46,7 +45,6 @@ int cjson_util_lookup(cJSON* root, cJSON** rv, const char* fmt, ...);
  * @param rv Recevies the node if successful.
  * @param fmt Path format string.
  * @param vargs Format args.
- * @note Pass root=NULL for the global root.
  */
 int cjson_util_vlookup(cJSON* root, cJSON** rv, const char* fmt, va_list vargs);
 
