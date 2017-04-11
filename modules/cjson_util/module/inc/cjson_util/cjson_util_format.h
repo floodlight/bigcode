158aaab122ad744197a9dcf561d2a90d42175d0a
Jeffrey Townsend <jeffrey.townsend@bigswitch.com>
2017-04-11 00:00:30 +0000

    Additional JSON Utilities
    - Support YAML output formatting.
      Structured interchange data should always be constructed in JSON.
      The output of that data is easier to consume by human beings in YAML.
    
      The YAML output formatter can be used to provide human-readable versions
      for all interchange data if necessary.
    
    - Formatting and construction utilities
      - Support AIM datatypes when adding strings.
      - Generate JSON objects for AIM flag definitions.

---
+++ modules/cjson_util/module/inc/cjson_util/cjson_util_format.h
@@ -0,0 +1,76 @@
+/************************************************************
+ * <bsn.cl fy=2017 v=epl>
+ *
+ *           Copyright 2017 Big Switch Networks, Inc.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *
+ * </bsn.cl>
+ ********************************************************//**
+ *
+ * cJSON Formatting Utilities
+ *
+ ***********************************************************/
+#ifndef __CJSON_UTIL_FORMAT_H__
+#define __CJSON_UTIL_FORMAT_H__
+
+#include <cjson_util/cjson_util_config.h>
+#include <AIM/aim_pvs.h>
+#include <cjson/cJSON.h>
+
+/**
+ * @brief Output JSON to YAML
+ * @param pvs The output pvs.
+ * @param root The CJSON root.
+ */
+int cjson_util_yaml_pvs(aim_pvs_t* pvs, cJSON* root);
+
+/**
+ * @brief Convert to YAML
+ * @param root The CJSON root.
+ */
+char* cjson_util_yaml_str(cJSON* root);
+
+/**
+ * @brief Output JSON
+ * @param pvs The output pvs.
+ * @param root The CJSON root.
+ */
+int cjson_util_json_pvs(aim_pvs_t* pvs, cJSON* root);
+
+/**
+ * @brief Convert to JSON string.
+ * @param The CJSON root.
+ */
+char* cjson_util_json_str(cJSON* root);
+
+/**
+ * @brief Create a flagmap JSON object.
+ * @param flags The flags
+ * @param map The simap
+ */
+
+cJSON* cjson_util_flags(uint32_t flags, aim_map_si_t* map);
+
+/**
+ * @brief Add a string with formatting to an object.
+ * @param item The object.
+ * @param name The name.
+ * @param fmt
+ * @param ... The format arguments.
+ */
+int cjson_util_add_string_to_object(cJSON* item, const char* name, const char* fmt, ...);
+int cjson_util_add_vstring_to_object(cJSON* item, const char* name, const char* fmt, va_list vargs);
+
+#endif /* __CJSON_UTIL_FORMAT_H__ */
---
+++ modules/cjson_util/module/src/cjson_util_format.c
@@ -0,0 +1,227 @@
+/************************************************************
+ * <bsn.cl fy=2017 v=epl>
+ *
+ *           Copyright 2017 Big Switch Networks, Inc.
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *        http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *
+ * </bsn.cl>
+ **********************************************************/
+
+#include <cjson_util/cjson_util_format.h>
+#include <IOF/iof.h>
+#include <math.h>
+#include <limits.h>
+#include <float.h>
+#include "cjson_util_log.h"
+#include <AIM/aim_pvs_buffer.h>
+
+static int format_yaml_type__(iof_t* iof, cJSON* root);
+
+static void
+format_yaml_string__(iof_t* iof, cJSON* item)
+{
+    iof_iprintf(iof, "%s%s%s",
+                item->string ? item->string : "",
+                item->string ? ": " : "",
+                item->valuestring);
+}
+
+static void
+format_yaml_number__(iof_t* iof, cJSON* item)
+{
+    double d = item->valuedouble;
+    if (fabs(((double)item->valueint)-d)<=DBL_EPSILON && d<=INT_MAX && d>=INT_MIN) {
+        iof_iprintf(iof, "%s%s%d",
+                    item->string ? item->string : "",
+                    item->string ? ": " : "",
+                    item->valueint);
+    }
+    else {
+        if (fabs(floor(d)-d)<=DBL_EPSILON && fabs(d)<1.0e60) {
+            iof_iprintf(iof, "%s%s%.0f",
+                        item->string ? item->string : "",
+                        item->string ? ": " : "",
+                        d);
+        }
+        else if (fabs(d)<1.0e-6 || fabs(d)>1.0e9) {
+            iof_iprintf(iof, "%s%s%e",
+                        item->string ? item->string : "",
+                        item->string ? ": " : "",
+                        d);
+        } else {
+            iof_iprintf(iof, "%s%s%f",
+                        item->string ? item->string : "",
+                        item->string ? ": " : "",
+                        d);
+        }
+    }
+}
+
+static void
+format_yaml_object__(iof_t* iof, cJSON* item)
+{
+    cJSON* p;
+
+    if(item->string) {
+        iof_push(iof, "%s", item->string);
+    }
+
+    for(p = item->child; p; p = p->next) {
+        format_yaml_type__(iof, p);
+    }
+
+    if(item->string) {
+        iof_pop(iof);
+    }
+}
+
+static void
+format_yaml_array__(iof_t* iof, cJSON* item)
+{
+    cJSON* p;
+
+    if(item->string) {
+        iof_push(iof, "%s", item->string);
+    }
+
+    for(p = item->child; p; p = p -> next) {
+        iof_iprintfn(iof, "- ");
+        format_yaml_type__(iof, p);
+    }
+
+    if(item->string) {
+        iof_pop(iof);
+    }
+}
+
+static void
+format_yaml_immediate__(iof_t* iof, cJSON* item, const char* s)
+{
+    iof_iprintf(iof, "%s%s%s",
+                item->string ? item->string : "",
+                item->string ? ": " : "",
+                s);
+}
+
+int
+format_yaml_type__(iof_t* iof, cJSON* root)
+{
+    int rv = 0;
+
+    if(root == NULL) {
+        return 0;
+    }
+
+    switch((root->type) & 0xFF)
+        {
+        case cJSON_False:
+            format_yaml_immediate__(iof, root, "False");
+            break;
+        case cJSON_True:
+            format_yaml_immediate__(iof, root, "True");
+            break;
+        case cJSON_NULL:
+            format_yaml_immediate__(iof, root, "None");
+            break;
+        case cJSON_String:
+            format_yaml_string__(iof, root);
+            break;
+        case cJSON_Number:
+            format_yaml_number__(iof, root);
+            break;
+        case cJSON_Object:
+            format_yaml_object__(iof, root);
+            break;
+        case cJSON_Array:
+            format_yaml_array__(iof, root);
+            break;
+
+        default:
+            AIM_LOG_ERROR("type not handled. (%d)", root->type & 0xFF);
+            rv = -1;
+        }
+    return rv;
+}
+
+int
+cjson_util_yaml_pvs(aim_pvs_t* pvs, cJSON* root)
+{
+    iof_t iof;
+
+    /** Initialize IOF for YAML formatting */
+    iof_init(&iof, pvs);
+    iof.push_string = ":";
+    iof.pop_string = NULL;
+    iof.indent_factor = 2;
+    return format_yaml_type__(&iof, root);
+}
+char*
+cjson_util_yaml_str(cJSON* root)
+{
+    char* rv;
+    aim_pvs_t* pvs = aim_pvs_buffer_create();
+    cjson_util_yaml_pvs(pvs,  root);
+    rv = aim_pvs_buffer_get(pvs);
+    aim_pvs_destroy(pvs);
+    return rv;
+}
+
+int
+cjson_util_json_pvs(aim_pvs_t* pvs, cJSON* root)
+{
+    char* str = cjson_util_json_str(root);
+    aim_printf(pvs, "%s", str);
+    aim_free(str);
+    return 0;
+}
+
+char*
+cjson_util_json_str(cJSON* root)
+{
+    return cJSON_Print(root);
+}
+
+cJSON*
+cjson_util_flags(uint32_t flags, aim_map_si_t* map)
+{
+    aim_map_si_t* p;
+    cJSON* rv = cJSON_CreateObject();
+    for(p = map; p->s; p++) {
+        cJSON_AddBoolToObject(rv, p->s, (flags & (p->i)));
+    }
+    return rv;
+}
+
+int
+cjson_util_add_string_to_object(cJSON* item, const char* name,
+                                const char* fmt, ...)
+{
+    int rv;
+    va_list vargs;
+    va_start(vargs, fmt);
+    rv = cjson_util_add_vstring_to_object(item, name, fmt, vargs);
+    va_end(vargs);
+    return rv;
+}
+
+int
+cjson_util_add_vstring_to_object(cJSON* item, const char* name,
+                                 const char* fmt, va_list vargs)
+{
+    char* str = aim_vdfstrdup(fmt, vargs);
+    cJSON_AddStringToObject(item, name, str);
+    aim_free(str);
+    return 0;
+}
